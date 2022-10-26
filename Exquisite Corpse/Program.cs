c313b4048f021b09c881c239eb1af27e54e14d1a
JoeWalton77 <josephwalton404@gmail.com>
2022-10-26 15:15:57 +0100

    first version

--- Exquisite Corpse/Program.cs
+++ Exquisite Corpse/Program.cs
@@ -6,7 +6,105 @@ namespace ExquisiteCorpse
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Do you want to create a random creature or customise it?");
+            Console.WriteLine("Type 1 for random or 2 for custom: ");
+            string userChoice = Console.ReadLine();
 
+            if (userChoice == "1")
+            { RandomMode(); }
+            else if (userChoice == "2")
+            {
+                //this part can be improved by allowing more flexibility with capitalisation when answering
+                Console.WriteLine("Should the creature have a ghost, bug or monster head?");
+                string userHead = Console.ReadLine();
+                Console.WriteLine("Should the creature have a ghost, bug or monster body?");
+                string userBody = Console.ReadLine();
+                Console.WriteLine("Should the creature have ghost, bug or monster feet?");
+                string userFeet = Console.ReadLine();
+
+                BuildACreature(userHead, userBody, userFeet);
+            }
+            
+        }
+
+        static void BuildACreature(string head, string body, string feet)
+        {
+            int headNum = TranslateToNumber(head);
+            int bodyNum = TranslateToNumber(body);
+            int feetNum = TranslateToNumber(feet);
+            SwitchCase(headNum, bodyNum, feetNum);
+        }
+
+        static void RandomMode()
+        {
+            Random randomNumber = new Random();
+            int randomHead = randomNumber.Next(1, 4);
+            int randomBody = randomNumber.Next(1, 4);
+            int randomFeet = randomNumber.Next(1, 4);
+            SwitchCase(randomHead, randomBody, randomFeet);
+        }
+
+        static void SwitchCase(int head, int body, int feet)
+        {
+            switch (head)
+            {
+                case 1:
+                    GhostHead();
+                    break;
+                case 2:
+                    BugHead();
+                    break;
+                case 3:
+                    MonsterHead();
+                    break;
+                default:
+                    break;
+            }
+
+            switch (body)
+            {
+                case 1:
+                    GhostBody();
+                    break;
+                case 2:
+                    BugBody();
+                    break;
+                case 3:
+                    MonsterBody();
+                    break;
+                default:
+                    break;
+            }
+
+            switch (feet)
+            {
+                case 1:
+                    GhostFeet();
+                    break;
+                case 2:
+                    BugFeet();
+                    break;
+                case 3:
+                    MonsterFeet();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        static int TranslateToNumber(string creature)
+        {
+            switch (creature)
+            {
+                case "ghost":
+                    return 1;
+                case "bug":
+                    return 2;
+                case "monster":
+                    return 3;
+                default:
+                    return 1;
+            }
         }
 
         static void GhostHead()
