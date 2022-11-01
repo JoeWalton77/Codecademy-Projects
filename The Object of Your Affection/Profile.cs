eca79dc216740ff6b1dd6428f19434c5596e6ca2
JoeWalton77 <josephwalton404@gmail.com>
2022-11-01 15:01:10 +0000

    first version
    practice with classes and objects

---
+++ The Object of Your Affection/Profile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace DatingProfile
+{
+    class Profile
+    {
+        private string name;
+        private int age;
+        private string city;
+        private string country;
+        private string pronouns;
+        private string[] hobbies;
+
+        public Profile(string name, int age, string city, string country, string pronouns = "they/them")
+        {
+            this.name = name;
+            this.age = age;
+            this.city = city;
+            this.country = country;
+            this.pronouns = pronouns;
+            this.hobbies = new string[0];
+        }
+
+
+        public string ViewProfile()
+        {
+            string userProfile = $"Name: {name}\nAge: {age}\nLocation: {city}, {country}\nPronouns: {pronouns}";
+            userProfile += "\nHobbies:\n";
+            foreach (string hobby in hobbies)
+            {
+                userProfile += $"- {hobby}\n";
+            }
+            return userProfile;
+            
+        }
+
+        public void SetHobbies(string[] hobbies)
+        {
+            this.hobbies = hobbies;
+        }
+    }
+}
---
+++ The Object of Your Affection/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DatingProfile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Profile sam = new Profile("Sam Drakilla", 30, "New York", "USA", "he/him");
+            sam.SetHobbies(new string[] { "Playing guitar", "Listening to music", "Reading fiction books" });
+            Console.WriteLine(sam.ViewProfile());
+        }
+    }
+}
+
+ 
