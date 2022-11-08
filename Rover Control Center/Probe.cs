47c95c67473ec27065da2d6cf7b9c3b2ab859699
JoeWalton77 <josephwalton404@gmail.com>
2022-11-08 15:50:43 +0000

    initial version

---
+++ Rover Control Center/Probe.cs
@@ -0,0 +1,21 @@
+﻿namespace RoverControlCenter
+{
+    class Probe
+    {
+        // class could be used to practice using a shared superclass instead of a shared interface, interface is used as it is more efficient
+        public virtual string GetInfo()
+        {
+            return "";
+        }
+
+        public virtual string Explore()
+        {
+            return "";
+        }
+
+        public virtual string Collect()
+        {
+            return "";
+        }
+    }
+}
---
+++ Rover Control Center/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace RoverControlCenter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MoonRover lunokhod = new MoonRover("Lunokhod 1", 1970);
+            MoonRover apollo = new MoonRover("Apollo 15", 1971);
+            MarsRover sojourner = new MarsRover("Sojourner", 1997);
+            Satellite sputnik = new Satellite("Sputnik", 1957);
+
+            IDirectable[] probes = { lunokhod, apollo, sojourner, sputnik };
+
+            /* foreach (IDirectable probe in probes)                 just checks that they are saved as the correct type
+             {
+                 Console.WriteLine(probe.GetType());    
+             } */
+
+            DirectAll(probes);
+        }
+
+        public static void DirectAll(IDirectable[] probes)
+        {
+            foreach (IDirectable probe in probes)
+            {
+                Console.WriteLine(probe.GetInfo());
+                Console.WriteLine(probe.Explore());
+                Console.WriteLine(probe.Collect());
+            }
+        }
+    }
+}
